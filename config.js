12d1821d3ef7390b4f7b1e2770ed8ae21895bb2f
raymondnorton-png <raymond.norton@swwc.org>
2026-08-28 14:00:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.CRF_SMS_FORMS = {
   optin: "https://docs.google.com/forms/d/e/1FAIpQLScj7m2GpGwrz2bDQsJwpTR78SbCdLF2g1fB15O_jpZvpdCdrA/viewform?usp=header",
   help: "https://docs.google.com/forms/d/e/1FAIpQLSdjPNTcBgmJHr1MJ8O6ARalfNJI3lZkc4YpR52JBdCwkZ0oqw/viewform?usp=header",
-  optout: "PASTE_OPTOUT_FORM_URL"
+  optout: "https://docs.google.com/forms/d/e/1FAIpQLScj7m2GpGwrz2bDQsJwpTR78SbCdLF2g1fB15O_jpZvpdCdrA/viewform?usp=header"
 };
