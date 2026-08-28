ef463ae67a59dfb0cf66565b87decd34199bebd1
raymondnorton-png <raymond.norton@swwc.org>
2026-08-28 12:35:47 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+/* Paste published viewform URLs from the spreadsheet tab "Form links". */
+window.CRF_SMS_FORMS = {
+  optin: "PASTE_OPTIN_FORM_URL",
+  help: "PASTE_HELP_FORM_URL",
+  optout: "PASTE_OPTOUT_FORM_URL"
+};
