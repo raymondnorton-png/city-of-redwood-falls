bb6f842b6c1655cc1d875a0ca788b4b8e867e7e1
raymondnorton-png <raymond.norton@swwc.org>
2026-08-28 14:00:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Paste published viewform URLs from the spreadsheet tab "Form links". */
 window.CRF_SMS_FORMS = {
   optin: "https://docs.google.com/forms/d/e/1FAIpQLScj7m2GpGwrz2bDQsJwpTR78SbCdLF2g1fB15O_jpZvpdCdrA/viewform?usp=header",
-  help: "PASTE_HELP_FORM_URL",
+  help: "https://docs.google.com/forms/d/e/1FAIpQLSdjPNTcBgmJHr1MJ8O6ARalfNJI3lZkc4YpR52JBdCwkZ0oqw/viewform?usp=header",
   optout: "PASTE_OPTOUT_FORM_URL"
 };
