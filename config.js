2a79af74c8a41536037c82f25ef0bd323af5832b
raymondnorton-png <raymond.norton@swwc.org>
2026-08-28 12:24:48 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+/* Paste the three published form URLs from the spreadsheet tab "Form links".
+   Use the public "viewform" links, not the edit links.
+   Example: https://docs.google.com/forms/d/e/1FAIpQLSd.../viewform
+*/
+window.CRF_SMS_FORMS = {
+  optin: "https://docs.google.com/forms/d/e/1FAIpQLScj7m2GpGwrz2bDQsJwpTR78SbCdLF2g1fB15O_jpZvpdCdrA/viewform",
+  help: "https://docs.google.com/forms/d/e/1FAIpQLSdjPNTcBgmJHr1MJ8O6ARalfNJI3lZkc4YpR52JBdCwkZ0oqw/viewform",
+  optout: "https://docs.google.com/forms/d/e/1FAIpQLSfVPHWvbadNSnpiDgO4D_ovCD4DkoEMmDB_ckCZbsKjiWlQDQ/viewform"
+};
